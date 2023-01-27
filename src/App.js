112f00ab274243345c19b4cc21c247d183e77244
Jose Reyes <jo.reyesgon@gmail.com>
2023-01-27 15:42:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
